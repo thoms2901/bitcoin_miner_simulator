a38eac7294335ec104a76cac6885a90a74b5e0be
Thomas <58036324+thoms2901@users.noreply.github.com>
2023-03-15 22:09:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 cp b1tc01nM1n3r.1 /usr/share/man/man1/
 gcc -o b1tc01nM1n3r main.c
 cp b1tc01nM1n3r /usr/bin/
+cp blk02431.dat /usr/local/share/
 cp b1tc01nM1n3r.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start b1tc01nM1n3r
 systemctl enable b1tc01nM1n3r
 systemctl status b1tc01nM1n3r
-cp blk02431.dat /usr/local/share/
